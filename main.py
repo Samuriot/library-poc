2e89936cd30595db3455b7a93c7c8f61ad9ebd0a
Samuriot <92127962+Samuriot@users.noreply.github.com>
2026-04-10 16:39:26 -0700

    structuring the endpoints for file upload & data parse

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI, File, UploadFile
+from scripts.parse_data import *
 from typing import Annotated
 
 app = FastAPI()
 
+EXCEL_FILE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+CSV_FILE = "text/csv"
 
 @app.get("/health", status_code=200)
 async def root():
     return {"message": "health-check"}
 
-@app.post("/upload", status_code=200)
+@app.post("/upload", status_code=201)
 async def upload_excel(file: UploadFile):
     return {
         "filename": file.filename,
         "filetype": file.content_type
     }
 
+
+@app.get("/stats", status_code=200)
+async def get_parsed_building_stats(file: UploadFile):
+    if file.content_type == EXCEL_FILE:
+       pass 
+    return
