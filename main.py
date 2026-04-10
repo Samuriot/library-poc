3740e956f77e52c577445812863f5c07a67c5370
Samuriot <92127962+Samuriot@users.noreply.github.com>
2026-04-10 16:23:08 -0700

    added endpoint for uploading files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from typing import Annotated
 
 app = FastAPI()
 
@@ -6,3 +7,11 @@ app = FastAPI()
 @app.get("/health", status_code=200)
 async def root():
     return {"message": "health-check"}
+
+@app.post("/upload", status_code=200)
+async def upload_excel(file: UploadFile):
+    return {
+        "filename": file.filename,
+        "filetype": file.content_type
+    }
+
